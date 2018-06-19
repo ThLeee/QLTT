24e0a78385d12a63ed010465e57337611406e2e7
TienRyo <boy96totdong@gmail.com>
2018-06-20 00:16:31 +0700

    create login

---
+++ seeds/credentail_data_sample.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('credentials').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('credentials').insert([
+        {id: 1, code: '1400659', password : '$2a$10$qi2ioQpBM2PZsSXVWnSXA.mYu3JQ6VzrcwF4w8mJpRpoClnem3Yha', role : 'intern'},
+        {id: 2, code: 'DHCNVH1', password : '$2a$10$qi2ioQpBM2PZsSXVWnSXA.mYu3JQ6VzrcwF4w8mJpRpoClnem3Yha', role : 'lecturer'},
+        {id: 3, code: 'adminManagement', password : '$2a$10$qi2ioQpBM2PZsSXVWnSXA.mYu3JQ6VzrcwF4w8mJpRpoClnem3Yha', role : 'admin'},
+      ]);
+    });
+};
