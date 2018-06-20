2566981b488a744a7dccf5695514f22b5ef5b088
LoveRain96 <ducdt@sphinx-software.com>
2018-06-20 14:29:48 +0700

    update councils

--- migrations/20180620141623_modify_table_councils.js
+++ migrations/20180620141623_modify_table_councils.js
@@ -5,7 +5,6 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-
     return knex.schema.table('councils', (table)=> {
         table.dropColumn('internship_name');
     })
