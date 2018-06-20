69cfbfc2580a41d165a58f5768b527ba91685a85
ThuyLe12101995 <thuy.leminh31@gmail.com>
2018-06-20 13:07:29 +0700

    update

--- http/middleware/import.js
+++ http/middleware/import.js
@@ -1,29 +1,28 @@
 let XLSX = require('xlsx');
 module.exports = function (request, response, next) {
-    console.log(request.file);
 
-    // let workbook = XLSX.readFile(request.file);
-    // let sheet_name_list = workbook.SheetNames;
-    //
-    // let data = [];
-    // sheet_name_list.forEach(function(y) {
-    //     let worksheet = workbook.Sheets[y];
-    //     let headers = {};
-    //     for(z in worksheet) {
-    //         if(z[0] === '!') continue;
-    //         let col = z.substring(0,1);
-    //         let row = parseInt(z.substring(1));
-    //         let value = worksheet[z].v;
-    //         if(row === 1) {
-    //             headers[col] = value;
-    //             continue;
-    //         }
-    //         if(!data[row]) data[row]={};
-    //         data[row][headers[col]] = value;
-    //     }
-    // });
-    // data.shift();
-    // data.shift();
-    // request.dataImport = data;
-    // next();
+    let workbook = XLSX.readFile(`/home/tien/WebStormProject/react/${request.file}`);
+    let sheet_name_list = workbook.SheetNames;
+
+    let data = [];
+    sheet_name_list.forEach(function(y) {
+        let worksheet = workbook.Sheets[y];
+        let headers = {};
+        for(z in worksheet) {
+            if(z[0] === '!') continue;
+            let col = z.substring(0,1);
+            let row = parseInt(z.substring(1));
+            let value = worksheet[z].v;
+            if(row === 1) {
+                headers[col] = value;
+                continue;
+            }
+            if(!data[row]) data[row]={};
+            data[row][headers[col]] = value;
+        }
+    });
+    data.shift();
+    data.shift();
+    request.dataImport = data;
+    next();
 };
